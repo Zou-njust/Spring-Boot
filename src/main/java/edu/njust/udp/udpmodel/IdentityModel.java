ad897954cd8edb33cec33e7ca12ab1ac3b733f48
senyahiro <2623777178@qq.com>
2021-12-08 09:27:16 +0800

    udp sender

---
+++ src/main/java/edu/njust/udp/udpmodel/IdentityModel.java
@@ -0,0 +1,11 @@
+package edu.njust.udp.udpmodel;
+
+public class IdentityModel {
+    public byte mbType;
+    public String mbID;
+    public String mbName;
+    public String airport;
+    public String country;
+    public byte method;
+    public byte probability;
+}
